b2ecf201524532f5dee87616faf7fae5ba17192f
Diego Montesinos <chesp.gommont@gmail.com>
2017-12-07 16:16:23 -0600

    Clean the JS code and break into functions

--- js/wordselector.js
+++ js/wordselector.js
@@ -1,77 +1,79 @@
-$(document).ready( function() {
+
+var toti;
+var totd;
+var averageI;
+var averageD;
+
+function onClickCargaTxt () {
+
+	var txti = $(this).data('txti');
+	$('.contenidosi').load('data/datosyabuenos/' + txti, function () {});
 	
-					var toti;
-					var totd;
-					var averageI;
-					var averageD;
-					
-				// carga los textos	aah
-				$(".cargatxt").click( function() {
-					var txti = $(this).data("txti");
-					$(".contenidosi").load("data/datosyabuenos/"+txti, function() {
-					})
-				// carga los textos inaugurales	
-					var txtd = $(this).data("txtd");
-					$(".contenidosd").load("data/datosyabuenos/"+txtd, function() {
-					})
-					
-					//lograr que los tots y keywords aparezcan en el dom
-					toti = $(this).data("toti");
-					document.getElementById(".totalvisi").innerHTML = toti;
-					
-					
-					totd = $(this).data("totd");
-					document.getElementById(".totalvisd").innerHTML = totd;
-
-				})
-				//carga las selecciones por categoría, suma las apariciones por clase y promedia con el total.
-				
-				$(".cargaunderlines").click( function() {
-					var cat = $(this).data("categoria");
-					$("."+cat+"i").addClass("underline--magical"); 		
-					$("."+cat).addClass("underline--magical--r");
-					var keywordI = $("."+cat+"i").length;
-					document.getElementById(".keywordvisi").innerHTML = keywordI;
-					var keywordD = $("."+cat).length;
-					document.getElementById(".keywordvisd").innerHTML = keywordD;
-					console.log(keywordI);
-					console.log(toti);
-					console.log(keywordD);
-					console.log(totd);
-					averageI = Number(((keywordI/toti)*100).toFixed(2));
-					document.getElementById("promediovisi").innerHTML = averageI;
-					averageD = Number(((keywordD/totd)*100).toFixed(2));
-					document.getElementById("promediovisd").innerHTML = averageD;
-					console.log(averageI);
-					console.log(averageD);
-
-				})
-				
-				//deselecciona las categorías
-				$(".reset").click( function() {
-					var cat = $(this).data("categoria");
-					$("."+cat+"i").removeClass("underline--magical"); 		
-					$("."+cat).removeClass("underline--magical--r"); 		
-
-				})
-				
-			
-				//sumar y restar el promedio
-				$(".subPerc").click( function() {
-					averageI-=0.01;	
-					console.log(averageI);
-					document.getElementById("contenidosi").style.wordSpacing -= '0.2';
-				})
-				
-				$(".addPerc").click( function() {
-					averageI+=0.01;	
-					console.log(averageD);
-					document.getElementById("contenidosd").style.wordSpacing += '0.2';
-				})
-				
+	// carga los textos inaugurales	
+	var txtd = $(this).data('txtd');
+	$('.contenidosd').load('data/datosyabuenos/' + txtd, function () {});
 	
+	//lograr que los tots y keywords aparezcan en el dom
+	toti = $(this).data('toti');
+	document.getElementById('.totalvisi').innerHTML = toti;
 
-				
-			})
-			
-			
+	totd = $(this).data('totd');
+	document.getElementById('.totalvisd').innerHTML = totd;
+}
+
+function onClickCargaUnderlines () {
+	var cat = $(this).data('categoria');
+	
+	$('.' + cat + 'i').addClass('underline--magical');
+	$('.' + cat).addClass('underline--magical--r');
+
+	var keywordI = $('.' + cat + 'i').length;
+	document.getElementById('.keywordvisi').innerHTML = keywordI;
+
+	var keywordD = $('.'+cat).length;
+	document.getElementById('.keywordvisd').innerHTML = keywordD;
+
+	console.log(keywordI);
+	console.log(toti);
+	console.log(keywordD);
+	console.log(totd);
+
+	averageI = Number(((keywordI/toti)*100).toFixed(2));
+	document.getElementById('promediovisi').innerHTML = averageI;
+
+	averageD = Number(((keywordD/totd)*100).toFixed(2));
+	document.getElementById('promediovisd').innerHTML = averageD;
+
+	console.log(averageI);	
+	console.log(averageD);
+}
+
+function onClickReset () {
+	var cat = $(this).data('categoria');
+
+	$('.' + cat + 'i').removeClass('underline--magical');
+	$('.' + cat).removeClass('underline--magical--r');
+}
+
+function onClickSubPerc () {
+	averageI -= 0.01;
+	console.log(averageI);
+	document.getElementById('contenidosi').style.wordSpacing -= '0.2';
+}
+
+function onClickAddPerc () {
+	averageI+=0.01;	
+	console.log(averageD);
+	document.getElementById('contenidosd').style.wordSpacing += '0.2';
+}
+
+$(document).ready(function () {
+	
+	// Configura los escuchas de eventos: asocia eventos con callbacks
+	$('.cargatxt').click(onClickCargaTxt);
+	$('.cargaunderlines').click(onClickCargaUnderlines);
+	$('.reset').click(onClickReset);
+	$('.subPerc').click(onClickSubPerc);
+	$('.addPerc').click(onClickAddPerc);
+	
+});
